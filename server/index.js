5e2679c88a8fc0c723ed04784430767e961ea6fa
ajose-malik <ajose.malik@gmail.com>
2022-08-13 16:25:14 -0400

    refactor index.js

--- server/index.js
+++ server/index.js
@@ -1,28 +1,11 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
-const keys = require('./config/keys')
+require('./services/passport')
+const mongoose = require('mongoose')
+const keys  = require('./config/keys')
 
 const app = express()
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('accesssToken', accessToken)
-		}
-	)
-)
+require('./routes/authRoutes')(app)
 
-app.get(
-	'/auth/google',
-	passport.authenticate('google', { scope: ['profile', 'email'] })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
+mongoose.connect(keys.mongoURI)
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`STARTED ON ${PORT}`))
