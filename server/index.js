ea28b0bcf615f51f89b6e6acd29a9699e5979af2
ajose-malik <ajose.malik@gmail.com>
2022-08-14 15:21:43 -0400

    complete google auth

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
-require('./models/Users')
+require('./models/User')
 require('./services/passport')
 
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+app.use(
+	cookieSession({
+		// Cookie Options
+		maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+		// maxAge: 24 * 60 * 60 * 1000 // 24 hours
+		keys: [keys.cookieKey]
+	})
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 require('./routes/authRoutes')(app)
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`STARTED ON ${PORT}`))
