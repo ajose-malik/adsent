e01b3fdff9012f9198f7537e4c81afb409a3a1fc
ajose-malik <ajose.malik@gmail.com>
2022-08-14 12:16:40 -0400

    implement save for model instance

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
-require('./services/passport')
 const mongoose = require('mongoose')
-const keys  = require('./config/keys')
+const keys = require('./config/keys')
+require('./models/Users')
+require('./services/passport')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 require('./routes/authRoutes')(app)
 
-mongoose.connect(keys.mongoURI)
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`STARTED ON ${PORT}`))
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,10 @@
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
+
 
 passport.use(
 	new GoogleStrategy(
@@ -10,7 +14,7 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accesssToken', accessToken)
+			new User({googleId: profile.id}).save()
 		}
 	)
 )
