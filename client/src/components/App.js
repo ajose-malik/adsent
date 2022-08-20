365bfcaf66e5a418d3f6b8fb28a8a643dced909a
ajose-malik <ajose.malik@gmail.com>
2022-08-20 10:40:51 -0400

    changes to client

---
+++ client/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+import * as actions from '../actions'
+import Header from './Header'
+
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser()
+	}
+	render() {
+		return (
+			<div className='container'>
+				<BrowserRouter>
+					<Header />
+				</BrowserRouter>
+			</div>
+		)
+	}
+}
+
+export default connect(null, actions)(App)
---
+++ client/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Header = ({ auth }) => {
+	const renderContent = () => {
+		switch (auth) {
+			case null:
+				return
+			case false:
+				return (
+					<li>
+						<a href='/auth/google'>Login with Google</a>
+					</li>
+				)
+			default:
+				return (
+					<li>
+						<a>Logout</a>
+					</li>
+				)
+		}
+	}
+	return (
+		<nav>
+			<div className='nav-wrapper'>
+				<a className='brand-logo'>Adsent</a>
+				<ul className='right'>{renderContent()}</ul>
+			</div>
+		</nav>
+	)
+}
+
+const mapStateToProps = ({ auth }) => {
+	return { auth }
+}
+export default connect(mapStateToProps)(Header)
