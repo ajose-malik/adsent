9911471ba6cb9ea50f11ce7641dc36156afddb6e
ajose-malik <ajose.malik@gmail.com>
2022-08-22 20:53:00 -0400

    add post route to charge clients

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,5 +2,12 @@ const keys = required('../config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
 
 module.exports = app => {
-	app.post('/api/stripe', (req, res) => {})
+	app.post('/api/stripe', async (req, res) => {
+		const charge = await stripe.charges.create({
+			amount: 500,
+			currency: 'usd',
+			description: 'Adsent for $5',
+			source: req.body.id
+		})
+	})
 }
