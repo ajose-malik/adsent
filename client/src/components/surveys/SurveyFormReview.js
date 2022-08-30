79210e5f317044c90c8b82689c7d96b881a22b95
ajose-malik <ajose.malik@gmail.com>
2022-08-30 18:38:41 -0400

    add submitSurvey to actions.js

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { formFields } from './formFields'
+import * as actions from '../../actions'
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 	const reviewFields = formFields.map(({ name, label }) => {
 		return (
 			<div key={name}>
@@ -16,8 +17,18 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
 		<>
 			<h5>Please confirm your entries</h5>
 			{reviewFields}
-			<button className='yellow darken-3 btn-flat' onClick={onCancel}>
+			<button
+				className='yellow darken-3 white-text btn-flat'
+				onClick={onCancel}
+			>
 				Back
+			</button>
+			<button
+				onClick={() => submitSurvey(formValues)}
+				className='green btn-flat right white-text'
+			>
+				<i className='material-icons right'>mail</i>
+				Send Survey
 			</button>
 		</>
 	)
@@ -26,4 +37,4 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
 function mapStateToProps({ form }) {
 	return { formValues: form.surveyForm.values }
 }
-export default connect(mapStateToProps)(SurveyFormReview)
+export default connect(mapStateToProps, actions)(SurveyFormReview)
